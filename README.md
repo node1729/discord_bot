fd71a3ed07d8378ca308c7f6f5098f1a059eef31
node1729 <node1729@gmail.com>
2019-11-24 12:57:50 -0600

    More Rewrite stuff

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # discord_bot
-This bot is as simple as I wanted to make it while still retaining the features I wanted.
+~~This bot is as simple as I wanted to make it while still retaining the features I wanted.~~
+This bot is no longer simple.
 
 To install, first you will need python 3 and you will need to run `pip install -r requirements.txt`
-
 In order to get the background task to properly run, you have to send the command `!move #<channel here>`
 
-A reminder example can be seen on line 78 of `discord_bot.py`
+# Commands
+
+Commands can be specified from a JSON file called `commands.json`
+The rules for creating a command in this format can be found in `rules.json` the items the lists are the valid values for these commands. Only one can be specified.
+
+
--- discord_bot.py
+++ discord_bot.py
@@ -27,7 +27,7 @@ try:
 except FileNotFoundError:
     print("building commands.json")
     outfile = open("commands.json", "w")
-    json.dump({"!ping": {"channel": -1, "return_type": "message", "data": "pong", "indexed": False, "users": "any"}}, outfile, indent=4)
+    json.dump({"!ping": {"channel": -1, "return_type": "message", "data": "pong", "indexed": False, "admin": False}}, outfile, indent=4)
     outfile.flush()
     outfile.close()
 commands_file = open("commands.json")
@@ -37,80 +37,55 @@ commands_file.close()
 class DiscordBot(discord.Client):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
-        self.bg_task = self.loop.create_task(self.background_timer())
-        self.config = self.loop.create_task(self.config_default_channel())
         
+    async def on_ready(self):
+        print("Logged in as {0}".format(self.user))
+    
     async def config_default_channel(self):
         await self.wait_until_ready()
-        
-        if not self.get_channel(settings["message_channel"]["default"]): # if the channel is not assigned properly,
-                                                                         # attempt to assign it to general
-            channel = discord.utils.get(self.get_all_channels(), name="general")
+
+        if not self.get_channel(settings["message_channel"]["default"]): # attempt to assign channel if default is invalid.            
+            channel = discord.utils.get(self.get_all_channels(), name="general") # default to general, fallback on first channel
             if channel:
                 await self.reconfig_channel(channel.id)
             else:
-                print("Falling back to first channel in get_all_channels()")
+                print("General not found, falling back to first detected channel")
                 channel = await self.get_all_channels()
                 channel = next(channel)
                 await self.reconfig_channel(channel.id)
-        
-    async def reconfig_channel(self, channel_id, category="message_channel", channel_in="default"): # return True if channel 
-                                                                                                    # is successfully changed
+                
+    async def reconfig_channel(self, channel_id, category="message_channel", channel_in="default"): # RETURN True if channel
+                                                                                                    # is successfully updated
         try:
             channel_id = int(channel_id)
         except ValueError:
-            print("Error finding channel")
             return False
-
+        
         if self.get_channel(channel_id):
             channel_id = int(channel_id)
             settings_file = open("settings.json", "w")
             settings[category][channel_in] = channel_id
             json.dump(settings, settings_file, indent=4)
-            settings_file.close()
             return True
-
         else:
-            print("Error finding channel")
             return False
-
-    async def on_ready(self):
-        print("Logged in as {0}".format(self.user))
-
-    """async def command_pong(self, message):
-            await message.channel.send("pong")"""
-                        
-    async def command_move(self, message, channel_in="default"):
-        if message.author.guild_permissions.administrator:
-            msg = re.split(" ", message.content, 2)
-            if len(msg) == 2: # move entire bot to a channel
-                channel_id = msg[1][2:-1]
-                await self.reconfig_channel(channel_id)
-                for key in settings["commands"]:
-                    await self.reconfig_channel(channel_id, "commands", key)
-                await client.get_channel(settings["message_channel"][channel_in]).send("successfully moved to this channel")            
-                
-            elif len(msg) == 3: # move a single command to a channel
-                channel_id = msg[2][2:-1]
-                command_move = msg[1]
-                await self.reconfig_channel(channel_id, category="commands", channel_in=command_move)
+        
+    ######################################################
+    # Functions for handling commands from commands.json #
+    ######################################################
     
-    #########################################################
-    # begin Functions to handle commands from commands.json # 
-    #########################################################
-    
-    async def command_simple_message(self, key, message, from_file=""): # command for sending a "message" from the commands.json file
+    # from_file refers to the file that is being fetched
+    async def command_simple_message(self, key, message, from_file=""): # command for sending a "message"
         if ((message.author.guild_permissions.administrator)
-             or (not message.author.guild_permissions.administrator and not commands[key]["admin"])):
+             or (not message.author.guild_permissions.administrator and not commands[key]["admin"])): # permissions for basic users and admin
             data = commands[key]["data"]
             if from_file:
                 data = from_file
             await client.get_channel(commands[key]["channel"]).send(data)
     
-    async def command_upload_file(self, key, message, from_file=""): # command for sending a "file" from the commands.json file
+    async def command_upload_file(self, key, message, from_file=""): # command for sending a "file"
         if ((message.author.guild_permissions.administrator)
-            or (not message.author.guild_permissions.administrator and not commands[key]["admin"])):
+             or (not message.author.guild_permissions.administrator and not commands[key]["admin"])):
             data = commands[key]["data"]
             if from_file:
                 data = from_file
@@ -118,62 +93,56 @@ class DiscordBot(discord.Client):
             await client.get_channel(commands[key]["channel"]).send(file=discord.File(fp))
             fp.close()
             
-#    async def command_add_command(self, key, message):
-        
-        
-    #######################################################
-    # end Functions to handle commands from commands.json #
-    #######################################################
+    ######################
+    # Built in functions #
+    ######################
     
-    async def on_message(self, message): # returns true if command is processed
-        if message.author != self.user: # ignore messages from the bot 
+    # add commands to commands.json (text only)
+    async def command_add_command(self, message):
+        if message == "!addcom":
+            await message.channel.send("invalid syntax!")
+            return False
+        else:
+            content = message.content[len("!addcom "):]
+            content = re.split(" ", content, maxsplit=1) # split into a list at the first space
+            commands[content[0]] = {"channel": message.channel.id, "return_type": "message", "data": content[1], "indexed": False,
+                                    "admin": False}
+            fp = open("commands.json", "w")
+            json.dump(commands, fp, sort_keys=True, indent=4)
+            fp.flush()
+            fp.close()
+            await message.channel.send("Successfully added command `{}` with content `{}`".format(content[0], content[1]))
+            
+    
+    ######################
+    # Message Processing #
+    ######################
+    
+    async def on_message(self, message): # returns true if the command is processed
+        if message.author != self.user: # ignore messages from self
             print("message from {0.author} in {0.channel}: {0.content}".format(message))
-            built_in_commands = {
-                                 #"!move": self.command_move
-                                 }
+            built_in_commands = {"!addcom": self.command_add_command}
+            
+            # process built in commands
             for key in built_in_commands:
                 if message.content.startswith(key + " ") or message.content == key:
                     await built_in_commands[key](message)
                     return True
-
+            # process commands.json commands
             for key in commands:
                 if message.content.startswith(key + " ") or message.content == key:
                     if commands[key]["return_type"] == "message":
                         await self.command_simple_message(key, message)
                     elif commands[key]["return_type"] == "file":
                         await self.command_upload_file(key, message)
+                        
+                    """ TODO Figure out how to implement this.
                     elif commands[key]["return_type"] == "message_from_file":
-                        await self.command_from_file(key, message, "message")
+                        await self.command_simple_message(key, message, )
                     elif commands[key]["return_type"] == "file_from_file":
                         await self.command_from_file(key, message, "file")
-            
-                        
-    async def on_time(self):
-        if self.get_channel(settings["message_channel"]["default"]):
-            channel = self.get_channel(settings["message_channel"]["default"])
-            await channel.send("sample text")
-        else:
-            print("invalid configuration for automatic messages")
-            
-    async def background_timer(self):
-        await self.wait_until_ready()
-        
-        channel = self.get_channel(settings["message_channel"]["default"])
-        while not self.is_closed():
-            current_time = time.asctime()
-            day_of_week = current_time[:3] # Sun, Mon, Tue, Wed, Thu, Fri, Sat
-            month = current_time[4:7]      # Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec
-            day = current_time[8:10]       # space padded 2 character number
-            hour = current_time[11:13]     # 0 padded 2 character number
-            minute = current_time[14:16]   # 0 padded 2 character number
-            second = current_time[17:19]   # 0 padded 2 character number
-            year = current_time[20:]       # 4 character number
-            
-            # example reminder for 09:00 MWF class
-            days = ["Mon", "Wed", "Fri"]
-            if day_of_week in days and hour == "09" and minute == "00" and second == "00":
-                await channel.send("Reminder")
-            await asyncio.sleep(1)
-
+                    """
+                    
+    
 client = DiscordBot()
 client.run(clientdict["token"])
